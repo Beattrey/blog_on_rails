48052710911d661f87bcdbda5500ad48b9fe73e0
beattrey <beattrey1@gmail.com>
2021-10-11 23:45:50 +0300

    add about and terms controller and page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   resource :contacts, only: [:new, :create], path_names: { :new => '' }
   resources :articles, only: [:new, :create], path_names: { :new => ''}
+  resources :terms, only: [:new], path_names: { :new => ''}
+  resources :about, only: [:new], path_names: { :new => ''}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
