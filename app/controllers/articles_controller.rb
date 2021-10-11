3e3a95283381610f1ed677a9d6c681d6f3929bf0
beattrey <beattrey1@gmail.com>
2021-10-11 17:58:46 +0300

    add contacts page and controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,15 @@
 class ArticlesController < ApplicationController
   def new
-
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    @article.save
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_09_193321) do
+ActiveRecord::Schema.define(version: 2021_10_10_161736) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.text "email"
+    t.text "message"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
