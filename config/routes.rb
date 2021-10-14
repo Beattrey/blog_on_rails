251dd6578b4d584468a41dd4e76ac562242e337e
beattrey <beattrey1@gmail.com>
2021-10-14 13:42:12 +0300

    change static page terms and about

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'home/index'
 
+  get 'terms' => 'pages#terms'
+  get 'about' => 'pages#about'
+
   resource :contacts, only: [:new, :create], path_names: { :new => '' }
   resources :articles
-  resource :terms, only: [:new], path_names: { :new => '' }
-  resource :about, only: [:new], path_names: { :new => '' }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
