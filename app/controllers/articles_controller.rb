2d4792e0b379a4a060a0d42e5191e4fbc941b95e
beattrey <beattrey1@gmail.com>
2021-10-14 13:28:41 +0300

    Revert "Implement articles#edit"
    This reverts commit 3bf809dedf88ebb2353a8153a8458f497ebd55e7.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,31 +9,18 @@ class ArticlesController < ApplicationController
   end
 
   def new
+
   end
 
   def create
     @article = Article.new(article_params)
 
-    if @article.save
+    if @article.valid?
+      @article.save
       redirect_to @article
     else
       render action: 'new'
     end
-  end
-
-  def edit
-    @article = Article.find(params[:id])
-  end
-
-  def update
-    @article = Article.find(params[:id])
-
-    if @article.update(article_params)
-      redirect_to @article
-    else
-      render action: 'edit'
-    end
-
   end
 
   private
