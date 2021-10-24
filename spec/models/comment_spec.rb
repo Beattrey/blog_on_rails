937266c5d1f835045915a4648dfb2b76869759f8
Beattrey <beattrey1@gmail.com>
2021-10-24 18:19:23 +0300

    Update article and comment spec tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 describe Comment do
-  it { should belong_to :article }
+  describe 'validation' do
+    it { should validate_presence_of :body }
+  end
+  describe 'associations' do
+    it { should belong_to :article }
+  end
 end
+
+
