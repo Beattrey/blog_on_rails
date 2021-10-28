4034a83e7e4e0823575427317df0821ede488961
Beattrey <beattrey1@gmail.com>
2021-10-28 13:06:31 +0300

    add validation to registration user + make uniq username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :articles
+  validates_uniqueness_of :username
+  validates_uniqueness_of :email
 end
--- spec/features/user_creates_article_spec.rb
+++ spec/features/user_creates_article_spec.rb
@@ -9,4 +9,15 @@ feature 'Article Creation' do
     visit new_article_path
     expect(page).to have_content I18n.t('article.new_article')
   end
+
+  scenario 'allows user to create article' do
+    visit new_article_path
+
+    fill_in 'Title', with: 'test title'
+    fill_in 'Text', with: 'test text'
+
+    click_button 'Publish'
+
+    expect(page).to have_content 'Author'
+  end
 end
